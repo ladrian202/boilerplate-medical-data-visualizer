e0159f213400abe2eb2ad3edad41d337fc961d13
Luis Gonzalez <l.adrian877@gmail.com>
2025-09-05 15:07:48 +0000

    prueba

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -60,7 +60,7 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
     corr = None
